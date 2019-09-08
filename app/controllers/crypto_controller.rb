9dd08c617288e68172ecfb431a9bacdad5103ea2
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-09 01:10:22 +0200

    500 crypto

--- app/controllers/crypto_controller.rb
+++ app/controllers/crypto_controller.rb
@@ -1,8 +1,14 @@
 class CryptoController < ApplicationController
   def index
     @request = CoinMarketCap.request(options: {limit: [0], start: 0})
+
     if @request == nil
       @request1 =  Cryptocompare::CoinList.all["Data"]
+    else
+      @request += CoinMarketCap.request(options: {limit: [0], start: 100})
+      @request += CoinMarketCap.request(options: {limit: [0], start: 200})
+      @request += CoinMarketCap.request(options: {limit: [0], start: 300})
+      @request += CoinMarketCap.request(options: {limit: [0], start: 400})
     end
 
     @coinlists = Cryptocompare::CoinList.all["Data"].sort.to_h

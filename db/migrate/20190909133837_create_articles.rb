211464d1949eadda257151ac06976922d26008d3
victorfouquet <victor.fouquet@gmail.com>
2019-09-09 16:29:11 +0200

    linking article and tag tables

--- db/migrate/20190909133837_create_articles.rb
+++ db/migrate/20190909133837_create_articles.rb
@@ -3,7 +3,8 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title
       t.text :content
-      
+      t.belongs_to :user, index: true
+      t.belongs_to :tag, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 2019_09_09_134333) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.bigint "user_id"
+    t.bigint "tag_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["tag_id"], name: "index_articles_on_tag_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|

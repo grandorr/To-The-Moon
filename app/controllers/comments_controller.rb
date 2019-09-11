5e5a45f0306b6750215c67c3b9af3b7a6b056c90
victorfouquet <victor.fouquet@gmail.com>
2019-09-11 22:25:41 +0200

    comments edit/update back

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,16 @@ class CommentsController < ApplicationController
 		redirect_back(fallback_location: root_path)
 	end
 
+	def edit
+		@comment = Comment.find(params[:id])
+	end
+	
+	def update
+		@comment = Comment.find(params[:id])
+		@comment.update_attribute(:content, params[:content])
+		redirect_to article_path(@comment.article.id)
+	end
+
 	def destroy
 		Comment.find(params[:id]).destroy
 		redirect_back(fallback_location: root_path)

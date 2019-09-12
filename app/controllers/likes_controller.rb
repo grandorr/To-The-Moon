734847e12365157a95e24ebc896a6e355d9eeaa3
victorfouquet <victor.fouquet@gmail.com>
2019-09-12 20:22:33 +0200

    like/unlike article

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,21 @@
 class LikesController < ApplicationController
+	def new
+	end
+
+	def create
+		if params[:article] != nil
+			Like.create(
+				user: current_user,
+				article: Article.find(params[:article].to_i)
+				)
+		end
+		redirect_to article_path(params[:article].to_i)
+	end
+
+	def destroy
+		if params[:article] != nil
+			current_user.likes.find_by(article: params[:article]).destroy
+		end
+		redirect_to article_path(params[:article].to_i)
+	end
 end

a928747fa0afe00b9ff85bc27e08d34ec0dee76d
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-12 18:49:55 +0200

    NON

--- app/controllers/cryptolist_controller.rb
+++ app/controllers/cryptolist_controller.rb
@@ -1,5 +1,8 @@
 class CryptolistController < ApplicationController
   def show
+    if current_user
+        @coin_tab = current_user.crypto_currencies.all
+    end
     @n = params[:id]
     @n = @n.to_i
   end
--- app/controllers/user_cryptos_controller.rb
+++ app/controllers/user_cryptos_controller.rb
@@ -4,11 +4,11 @@ class UserCryptosController < ApplicationController
 		@cryptos = current_user.crypto_currencies
 		@user_cryptos = current_user.user_cryptos
 	end
-	
+
 	def create
 		unless CryptoCurrency.where(name: params[:name]).length == 0
 			UserCrypto.create(
-				user: current_user, 
+				user: current_user,
 				crypto_currency: CryptoCurrency.find_by(name: params[:name]),
 				quantity: 0
 				)
@@ -18,15 +18,21 @@ class UserCryptosController < ApplicationController
 				crypto_id: params[:id]
 				)
 			UserCrypto.create(
-				user: current_user, 
-				crypto_currency: @crypto
+				user: current_user,
+				crypto_currency: @crypto,
+				quantity: 0
+
 				)
 		end
-		
+
 	end
-	
+
 	def update
-		current_user.user_cryptos.find(params[:id]).update_attribute(:quantity, params[:quantity])
+
+		crypto_currency = CryptoCurrency.find_by(crypto_id: params[:id])
+		current_user.user_cryptos.find_by(crypto_currency: crypto_currency).update_attribute(:quantity, params[:quantity])
+		
+
 		redirect_back(fallback_location: root_path)
 	end
 

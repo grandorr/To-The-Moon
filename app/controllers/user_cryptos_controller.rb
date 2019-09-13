12e46265d196d61b77093d93ef8a3ff168f67688
victorfouquet <victor.fouquet@gmail.com>
2019-09-13 18:15:04 +0200

    reshaping fat model skinny controller

--- app/controllers/user_cryptos_controller.rb
+++ app/controllers/user_cryptos_controller.rb
@@ -1,7 +1,9 @@
 class UserCryptosController < ApplicationController
 	before_action :authenticate_user!
+
 	def index
 		@cryptos = current_user.crypto_currencies
+		@coins = UserCrypto.get_coins(@cryptos, current_user)
 		@user_cryptos = current_user.user_cryptos
 	end
 
@@ -22,18 +24,13 @@ class UserCryptosController < ApplicationController
 				user: current_user,
 				crypto_currency: @crypto,
 				quantity: 0
-
 				)
 		end
-
 	end
 
 	def update
-
 		crypto_currency = CryptoCurrency.find_by(crypto_id: params[:id])
 		current_user.user_cryptos.find_by(crypto_currency: crypto_currency).update_attribute(:quantity, params[:quantity])
-
-
 		redirect_back(fallback_location: root_path)
 	end
 

ef5657d44c6e9600293928c2a4553646a9a6e21e
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-13 18:32:22 +0200

    Portfolio clean view

--- app/controllers/user_cryptos_controller.rb
+++ app/controllers/user_cryptos_controller.rb
@@ -1,8 +1,27 @@
 class UserCryptosController < ApplicationController
 	before_action :authenticate_user!
+
+def get_coin
+	@coin = []
+	@crypto_currency = []
+	@cryptos = current_user.crypto_currencies
+	@cryptos.each do |crypto|
+	@crypto_currency << current_user.user_cryptos.find_by(crypto_currency: crypto)
+	url = 'https://api.coinlore.com/api/ticker/?id=' + crypto.crypto_id.to_s
+	@coin << api_request(url)
+
+	end
+	@crypto_array = [@coin,@crypto_currency]
+	return @crypto_array
+end
+
 	def index
-		@cryptos = current_user.crypto_currencies
+			crypto_array = get_coin
+		  @coin = crypto_array[0]
+		  @cryto_currency = crypto_array[1] 
+
 		@user_cryptos = current_user.user_cryptos
+
 	end
 
 

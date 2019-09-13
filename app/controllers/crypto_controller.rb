95ed6b215268913733f0184100f22c5fb5da4b33
victorfouquet <victor.fouquet@gmail.com>
2019-09-13 14:12:32 +0200

    reshaping fat model skinny controller (cryptos/friend_requests/friendships)

--- app/controllers/crypto_controller.rb
+++ app/controllers/crypto_controller.rb
@@ -1,23 +1,11 @@
 class CryptoController < ApplicationController
 
-  require 'net/http'
-  def api_request(url)
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    response = JSON.parse(response)
-    return response
-  end
-
-  def index
-  end
-
   def show
-
     @coin_id = params[:id]
     @coin_symbol = params[:symbol]
     @coin_name = params[:name]
     @url = 'https://api.coinlore.com/api/ticker/?id=' + @coin_id
-    @coin = api_request(@url)
+    @coin = Crypto.api_request(@url)
     @coin.each do |coin|
       if @coin_symbol == nil
         @coin_symbol = coin["symbol"]

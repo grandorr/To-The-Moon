495327dd858093d31c0ec6c4ab3cf84cf93b23e9
victorfouquet <victor.fouquet@gmail.com>
2019-09-15 19:10:00 +0200

    adding JS for friend requests and friendships functionalities

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -6,8 +6,10 @@ class FriendRequestsController < ApplicationController
 			user: @user,
 			pending_friend: current_user
 			)
-
-	  redirect_to profile_path(@user.id) 
+		respond_to do |format|
+    	format.html { redirect_back(fallback_location: root_path) }
+    	format.js { }
+  	end
 
 	end
 

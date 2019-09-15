66db63f02d4f969aedf7bd82bc5187faf97be590
victorfouquet <victor.fouquet@gmail.com>
2019-09-15 21:05:40 +0200

    implementing JS to add a crypto to portfolio

--- app/controllers/user_cryptos_controller.rb
+++ app/controllers/user_cryptos_controller.rb
@@ -10,12 +10,17 @@ class UserCryptosController < ApplicationController
 
 
 	def create
+		@n = params[:n].to_i
 		crypto_currency = CryptoCurrency.crypto_exists?(params[:name],  params[:id])
 		UserCrypto.create(
 			user: current_user,
 			crypto_currency: crypto_currency,
 			quantity: 0
-			)
+		)
+		respond_to do |format|
+    	format.html { redirect_back(fallback_location: root_path) }
+    	format.js { }
+  	end
 	end
 
 	def update

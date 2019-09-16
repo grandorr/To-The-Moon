016476249de826469c27bdcc3a91f318f8e05b8e
victorfouquet <victor.fouquet@gmail.com>
2019-09-16 14:02:39 +0200

    update quantity from portfolio view with JS

--- app/controllers/user_cryptos_controller.rb
+++ app/controllers/user_cryptos_controller.rb
@@ -12,7 +12,7 @@ class UserCryptosController < ApplicationController
 	def create
 		@n = params[:n].to_i
 		crypto_currency = CryptoCurrency.crypto_exists?(params[:name],  params[:id])
-		UserCrypto.create(
+		@crypto = UserCrypto.create(
 			user: current_user,
 			crypto_currency: crypto_currency,
 			quantity: 0
@@ -24,10 +24,20 @@ class UserCryptosController < ApplicationController
 	end
 
 	def update
+		puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+		puts(params)
+		puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 		crypto_currency = CryptoCurrency.find_by(crypto_id: params[:id])
 		quantity = UserCrypto.check_quantity(params[:quantity].to_i)	
-		current_user.user_cryptos.find_by(crypto_currency: crypto_currency).update_attribute(:quantity, quantity)
-		redirect_back(fallback_location: root_path)
+		@n = params[:n].to_i
+		@quantity = params[:quantity].to_f
+		@user_crypto = current_user.user_cryptos.find_by(crypto_currency: crypto_currency)
+		@user_crypto.update_attribute(:quantity, quantity)
+		
+		respond_to do |format|
+    	format.html { redirect_back(fallback_location: root_path) }
+    	format.js { }
+  	end
 	end
 
 	def destroy

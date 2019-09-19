9347e0d780c7931c44f3d99dafe6d1e64621658f
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-19 13:15:03 +0200

    search bar for user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,17 +5,24 @@ class ProfilesController < ApplicationController
 	end
 
 	def show
-		@user = User.find(params[:id])
-		@articles = @user.articles.reverse
-		@pending_friends = current_user.pending_friends
-		@friends = current_user.friends
-		@friendship = current_user.friendships.find_by(friend: @user)
-		@conversations = @user.conversations
-		@messages = @user.received_messages
-		message_infos = User.has_unread_messages?(@messages)
-		@senders = message_infos[0]
-		@unread_messages = message_infos[1]
-		@favorite_articles = current_user.favorite_articles
+		puts params
+		if params[:email] != nil
+			@user = User.find_by(email: params[:email])
+		else
+			@user = User.find(params[:id])
+		end
+		if @user != nil
+			@articles = @user.articles.reverse
+			@pending_friends = current_user.pending_friends
+			@friends = current_user.friends
+			@friendship = current_user.friendships.find_by(friend: @user)
+			@conversations = @user.conversations
+			@messages = @user.received_messages
+			message_infos = User.has_unread_messages?(@messages)
+			@senders = message_infos[0]
+			@unread_messages = message_infos[1]
+			@favorite_articles = current_user.favorite_articles
+		end 
 	end
 
 end

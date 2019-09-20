3eaf9547807349d7fb21d0a919f9ecf91485d788
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-09-20 16:29:58 +0200

    message

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,20 @@
 class ConversationsController < ApplicationController
 	before_action :authenticate_user!
-	
+
 	def show
-		@conversation = Conversation.find(params[:id])
-		@messages = @conversation.messages
-		Message.has_been_read?(@messages)
+		 if Conversation.exists?(id: params[:id])
+
+			@conversation = Conversation.find(params[:id])
+			@messages = @conversation.messages
+			Message.has_been_read?(@messages)
+  	else
+			id = params[:id].to_i
+			id -= 1
+			@conversation = Conversation.find(id)
+
+			@messages = @conversation.messages
+			Message.has_been_read?(@messages)
+		end
 	end
 
 end

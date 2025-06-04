3f7456dda414f1553701b6e082124ffadef01e03
Jo thomé Spada <joshuanspada@hotmail.fr>
2025-06-04 14:49:10 +0200

    Fix association for CryptoCurrency users

--- app/models/crypto_currency.rb
+++ app/models/crypto_currency.rb
@@ -1,6 +1,9 @@
 class CryptoCurrency < ApplicationRecord
 	has_many :user_cryptos
-	has_many :users, through: :user_cryptos, foreign_key: 'crypto_id'
+        # Link crypto currencies to the users owning them through the join model
+        # The user_cryptos table already uses `crypto_currency_id` so we don't
+        # need to specify a foreign_key here (and using `crypto_id` was wrong)
+        has_many :users, through: :user_cryptos
 
 	def self.crypto_exists?(name, id)
 		unless CryptoCurrency.where(name: name).length == 0
